ab754fc289b24591d05dc4a29f23d4cd26206bc5
Sehyo Chang <sehyo@nginx.com>
2018-04-04 16:11:24 -0700

    add rest of NGINX module options

--- ngx-sys/build.rs
+++ ngx-sys/build.rs
@@ -4,16 +4,100 @@ use std::process::Command;
 use std::process::Output;
 use std::env;
 use std::io::Result;
+use std::vec::Vec;
 
 const NGIX_DIR: &str  = "./nginx/";
 
-// perform make with argument
-fn make(arg: &str) -> Result<Output> {
+// return all nginx features
+fn ngix_features() -> Vec<&'static str> {
+    let mut features: Vec<&'static str> = Vec::new();
+    if cfg!(feature = "with-compat") {
+        features.push("--with-compat");
+    }
+    if cfg!(feature = "with-threads") {
+        features.push("--with-threads");
+    }
+    if cfg!(feature = "with-http_addition_module") {
+        features.push("--with-http_addition_module");
+    }      
+    if cfg!(feature = "with-http_auth_request_module") {
+        features.push("--with-http_auth_request_module");
+    }
+    if cfg!(feature = "with-http_gunzip_module") {
+        features.push("--with-http_gunzip_module");
+    }
+    if cfg!(feature = "with-http_auth_request_module") {
+        features.push("--with-http_auth_request_module");
+    }
+    if cfg!(feature = "with-http_gzip_static_module") {
+        features.push("--with-http_gzip_static_module");
+    }
+    if cfg!(feature = "with-http_random_index_module") {
+        features.push("--with-http_random_index_module");
+    }
+    if cfg!(feature = "with-http_realip_module") {
+        features.push("--with-http_realip_module");
+    }  
+    if cfg!(feature = "with-http_secure_link_module") {
+        features.push("--with-http_secure_link_module");
+    }    
+    if cfg!(feature = "with-http_slice_module") {
+        features.push("--with-http_slice_module");
+    }
+    if cfg!(feature = "with-http_stub_status_module") {
+        features.push("--with-http_stub_status_module");
+    } 
+    if cfg!(feature = "with-http_sub_module") {
+        features.push("--with-http_sub_module");
+    }  
+    if cfg!(feature = "with-stream") {
+        features.push("--with-stream");
+    }  
+    if cfg!(feature = "with-stream_realip_module") {
+        features.push("--with-stream_realip_module");
+    }
+    if cfg!(feature = "with-stream_ssl_preread_module") {
+        features.push("--with-stream_ssl_preread_module");
+    }  
+    
+    if cfg!(all(feature = "with-file-aio", target_os="linux")) {
+        features.push("--with-file-aio");
+    }
+    if cfg!(all(feature = "with-file-aio", target_os="linux"))  {
+        features.push("--with-http_ssl_module");
+    }
+    if cfg!(all(feature = "with-file-aio", target_os="linux"))  {
+        features.push("--with-stream_ssl_module");
+    } 
+
+
+    if cfg!(target_os="linux")  {
+        features.push("--with-cc-opt='-g -fstack-protector-strong -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -fPIC'");
+        features.push("--with-ld-opt='-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,-z,now -Wl,--as-needed -pie'");
+    } 
+    
+
+    println!("configuring nginx: {:?}",features);
+
+    features
+    
+}
+
+fn configure() -> Result<Output> {
     let current_path = env::current_dir().unwrap();
-    let path_name = format!("{}",current_path.display());
+    let path_name = format!("{}/nginx",current_path.display());
     println!("executing make command at {}",path_name);
-    let result =  Command::new("/usr/bin/make")
-        .args(&[arg])
+    let mut args: Vec<&str> = Vec::new();
+    args.push("auto/configure");
+    
+    let features = ngix_features();
+    
+    for feature in features {
+        args.push(&feature)
+    }
+        
+    let result =  Command::new("bash")
+        .args(&args)
         .current_dir(path_name)
         .output();
 
@@ -29,12 +113,10 @@ fn make(arg: &str) -> Result<Output> {
             return Ok(output);
         }
     }
+    
+    
 }
 
-
-fn configure() -> Result<Output> {
-    make("configure")
-}
 
 
 
