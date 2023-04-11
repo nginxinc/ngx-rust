f23e4b1688c4b520b74677c88aa02e352034e065
Maxim Ivanitskiy <m.ivanitskiy@f5.com>
2023-04-11 15:32:11 -0700

    fix: nginx-sys enable a few modules by default

--- nginx-sys/build.rs
+++ nginx-sys/build.rs
@@ -46,28 +46,31 @@ const ALL_SERVERS_AND_PUBLIC_KEY_IDS: [(&str, &str); 4] = [
     NGX_GPG_SERVER_AND_KEY_ID,
 ];
 /// List of configure switches specifying the modules to build nginx with
-const NGX_BASE_MODULES: [&str; 15] = [
+const NGX_BASE_MODULES: [&str; 20] = [
     "--with-compat",
-    "--with-threads",
     "--with-http_addition_module",
     "--with-http_auth_request_module",
+    "--with-http_flv_module",
     "--with-http_gunzip_module",
     "--with-http_gzip_static_module",
     "--with-http_random_index_module",
     "--with-http_realip_module",
     "--with-http_secure_link_module",
     "--with-http_slice_module",
+    "--with-http_slice_module",
+    "--with-http_ssl_module",
     "--with-http_stub_status_module",
     "--with-http_sub_module",
-    "--with-stream",
+    "--with-http_v2_module",
     "--with-stream_realip_module",
+    "--with-stream_ssl_module",
     "--with-stream_ssl_preread_module",
+    "--with-stream",
+    "--with-threads",
 ];
 /// Additional configuration flags to use when building on Linux.
-const NGX_LINUX_ADDITIONAL_OPTS: [&str; 5] = [
+const NGX_LINUX_ADDITIONAL_OPTS: [&str; 3] = [
     "--with-file-aio",
-    "--with-http_ssl_module",
-    "--with-stream_ssl_module",
     "--with-cc-opt=-g -fstack-protector-strong -Wformat -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -fPIC",
     "--with-ld-opt=-Wl,-Bsymbolic-functions -Wl,-z,relro -Wl,-z,now -Wl,--as-needed -pie",
 ];
