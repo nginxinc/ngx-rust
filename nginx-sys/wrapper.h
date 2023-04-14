2a51617c78c1aaee84190c5102303b2af1308af9
Ava Hahn <a.hahn@f5.com>
2023-04-14 16:55:41 -0700

    add event bindings
    Signed-off-by: Ava Hahn <a.hahn@f5.com>

--- nginx-sys/wrapper.h
+++ nginx-sys/wrapper.h
@@ -9,12 +9,3 @@ const size_t NGX_RS_HTTP_SRV_CONF_OFFSET = NGX_HTTP_SRV_CONF_OFFSET;
 const size_t NGX_RS_HTTP_LOC_CONF_OFFSET = NGX_HTTP_LOC_CONF_OFFSET;
 
 const char *NGX_RS_MODULE_SIGNATURE = NGX_MODULE_SIGNATURE;
-
-// Wrappers for inline functions here
-void ngx_add_ev_timer(ngx_event_t *ev, ngx_msec_t time) {
-  ngx_add_timer(ev, time);
-}
-
-void ngx_del_ev_timer(ngx_event_t *ev) {
-  ngx_event_del_timer(ev);
-}
