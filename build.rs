d368a2898dac57d36068ce27c633cc4c787eafc3
Aleksei Bavshin <a.bavshin@nginx.com>
2024-11-14 08:30:22 -0800

    feat: expose DEP_NGINX_ vars to the high-level bindings
    The example build script converts `DEP_NGINX_FEATURES`, `DEP_NGINX_OS`
    and `DEP_NGINX_VERSION_NUMBER` received from `nginx-sys` to the `cfg`
    values for conditional compilation.
    
    See tests for usage examples.

---
+++ build.rs
@@ -0,0 +1,46 @@
+/// Example buildscript for an nginx module.
+///
+/// Due to the limitations of cargo[1], this buildscript _requires_ adding `nginx-sys` to the
+/// direct dependencies of your crate.
+///
+/// [1]: https://github.com/rust-lang/cargo/issues/3544
+fn main() {
+    // Generate `ngx_os` and `ngx_feature` cfg values
+
+    // Specify acceptable values for `ngx_feature`
+    println!("cargo::rerun-if-env-changed=DEP_NGINX_FEATURES_CHECK");
+    println!(
+        "cargo::rustc-check-cfg=cfg(ngx_feature, values({}))",
+        std::env::var("DEP_NGINX_FEATURES_CHECK").unwrap_or("any()".to_string())
+    );
+    // Read feature flags detected by nginx-sys and pass to the compiler.
+    println!("cargo::rerun-if-env-changed=DEP_NGINX_FEATURES");
+    if let Ok(features) = std::env::var("DEP_NGINX_FEATURES") {
+        for feature in features.split(',').map(str::trim) {
+            println!("cargo::rustc-cfg=ngx_feature=\"{}\"", feature);
+        }
+    }
+
+    // Specify acceptable values for `ngx_os`
+    println!("cargo::rerun-if-env-changed=DEP_NGINX_OS_CHECK");
+    println!(
+        "cargo::rustc-check-cfg=cfg(ngx_os, values({}))",
+        std::env::var("DEP_NGINX_OS_CHECK").unwrap_or("any()".to_string())
+    );
+    // Read operating system detected by nginx-sys and pass to the compiler.
+    println!("cargo::rerun-if-env-changed=DEP_NGINX_OS");
+    if let Ok(os) = std::env::var("DEP_NGINX_OS") {
+        println!("cargo::rustc-cfg=ngx_os=\"{}\"", os);
+    }
+
+    // Generate cfg values for version checks
+    // println!("cargo::rustc-check-cfg=cfg(nginx1_27_0)");
+    // println!("cargo::rerun-if-env-changed=DEP_NGINX_VERSION_NUMBER");
+    // if let Ok(version) = std::env::var("DEP_NGINX_VERSION_NUMBER") {
+    //     let version: u64 = version.parse().unwrap();
+    //
+    //     if version >= 1_027_000 {
+    //         println!("cargo::rustc-cfg=nginx1_27_0");
+    //     }
+    // }
+}
