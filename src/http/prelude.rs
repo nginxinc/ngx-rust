598c8c3e594cfc0df49fe80561cd2c445823f747
Dmitry Volyntsev <xeioex@nginx.com>
2025-01-28 22:38:10 -0800

    feat: expose minimal sufficient set of symbols via ::prelude::*

---
+++ src/http/prelude.rs
@@ -0,0 +1,23 @@
+pub use crate::ffi::ngx_http_handler_pt;
+pub use crate::ffi::ngx_http_module_t;
+pub use crate::ffi::ngx_http_request_t;
+pub use crate::ffi::ngx_http_variable_t;
+pub use crate::ffi::ngx_http_variable_value_t;
+
+pub use crate::ffi::NGX_HTTP_MODULE;
+
+pub use crate::ffi::NGX_HTTP_LIF_CONF;
+pub use crate::ffi::NGX_HTTP_LMT_CONF;
+pub use crate::ffi::NGX_HTTP_LOC_CONF;
+pub use crate::ffi::NGX_HTTP_MAIN_CONF;
+pub use crate::ffi::NGX_HTTP_SIF_CONF;
+pub use crate::ffi::NGX_HTTP_SRV_CONF;
+pub use crate::ffi::NGX_HTTP_UPS_CONF;
+
+pub use crate::ffi::NGX_HTTP_LOC_CONF_OFFSET;
+pub use crate::ffi::NGX_HTTP_MAIN_CONF_OFFSET;
+pub use crate::ffi::NGX_HTTP_SRV_CONF_OFFSET;
+
+pub use crate::ffi::ngx_http_core_module;
+
+pub use crate::ffi::ngx_http_add_variable;
